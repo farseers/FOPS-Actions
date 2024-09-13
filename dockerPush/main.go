22dbde947df686ea56312d716c0a56e9b164f771
steden <1470804@qq.com>
2024-09-13 12:02:16 +0800

    镜像登陆、推送增加重试5次功能

--- dockerPush/main.go
+++ dockerPush/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/farseer-go/utils/exec"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -15,19 +16,22 @@ func main() {
 	// 上传完后，删除本地镜像
 	defer exec.RunShellCommand("docker rmi "+With.DockerImage, nil, "", true)
 
-	// 上传
-	var result = exec.RunShell("docker push "+With.DockerImage, progress, nil, "", true)
-	if result == 0 {
-		progress <- "镜像上传完成。"
+	// 重试5次
+	for tryCount := 0; tryCount < 5; tryCount++ {
+		// 上传
+		var result = exec.RunShell("docker push "+With.DockerImage, progress, nil, "", true)
+		if result == 0 {
+			progress <- "镜像上传完成。"
+			waitProgress()
+			return
+		}
+		time.Sleep(3 * time.Second)
+		progress <- fmt.Sprintf("尝试第%d次推送\n", tryCount+1)
 	}
-
 	// 等待退出
 	waitProgress()
-
-	if result != 0 {
-		fmt.Println("镜像上传出错了")
-		os.Exit(-1)
-	}
+	fmt.Println("镜像上传出错了")
+	os.Exit(-1)
 }
 
 func loginDockerHub() {
@@ -37,12 +41,18 @@ func loginDockerHub() {
 		if !strings.Contains(With.DockerHub, ".") {
 			dockerHub = ""
 		}
-		var result = exec.RunShell("docker login "+dockerHub+" -u "+With.DockerUserName+" -p "+With.DockerUserPwd, progress, nil, "", true)
-		if result != 0 {
-			fmt.Println("镜像仓库登陆失败。")
-			os.Exit(-1)
-		}
-	}
 
-	progress <- "镜像仓库登陆成功。"
+		// 重试5次
+		for tryCount := 0; tryCount < 5; tryCount++ {
+			result := exec.RunShell("docker login "+dockerHub+" -u "+With.DockerUserName+" -p "+With.DockerUserPwd, progress, nil, "", true)
+			if result == 0 {
+				progress <- "镜像仓库登陆成功。"
+				return
+			}
+			time.Sleep(3 * time.Second)
+			progress <- fmt.Sprintf("尝试第%d次登陆\n", tryCount+1)
+		}
+		fmt.Println("镜像仓库登陆失败。")
+		os.Exit(-1)
+	}
 }
