4e6a4a502137541eb3d992e761db777b2223db63
steden <1470804@qq.com>
2024-09-13 12:03:12 +0800

    取消git安装判断。镜像默认安装了

--- gitProxy/main.go
+++ gitProxy/main.go
@@ -10,14 +10,15 @@ import (
 func main() {
 	go printProgress()
 
-	setupGit()
+	//setupGit()
 
 	if With.Proxy == "" {
 		fmt.Println(flog.Red("未设置proxy"))
 		os.Exit(-1)
 	}
 
-	cmd := fmt.Sprintf("git config --global http.https://github.com.proxy %s && git config --global https.https://github.com.proxy %s", With.Proxy, With.Proxy)
+	//cmd := fmt.Sprintf("git config --global git config --global https.proxy  %s && git config --global https.https://github.com.proxy %s", With.Proxy, With.Proxy)
+	cmd := fmt.Sprintf("git config --global https.proxy %s && git config --global http.proxy %s && git config --global https.sslVerify false && git config --global http.sslVerify false", With.Proxy, With.Proxy)
 	exec.RunShell(cmd, progress, nil, "", true)
 
 	// 等待退出
