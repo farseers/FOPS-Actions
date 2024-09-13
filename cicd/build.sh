2937f009a8cf0dc2cfe22b3deae09424add46ce8
steden <1470804@qq.com>
2024-09-13 12:02:46 +0800

    修改脚本

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,2 +1,3 @@
-docker build -t steden88/cicd:2.0 --network=host -f ./Dockerfile .
-docker push steden88/cicd:2.0
+export https_proxy=http://192.168.1.88:7890 http_proxy=http://192.168.1.88:7890 all_proxy=socks5://192.168.1.88:7890
+docker build -t steden88/cicd:3.0 --network=host --build-arg HTTP_PROXY=http://192.168.1.88:7890 --build-arg HTTPS_PROXY=http://192.168.1.88:7890 -f ./Dockerfile .
+docker push steden88/cicd:3.0
